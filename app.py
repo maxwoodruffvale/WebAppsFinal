c42bb93dcf581b44e602dca3fdbc83400a3318dc
ruffV <maxwellvale@icloud.com>
2024-05-01 13:14:01 -0500

    Create flask app and start home page

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+import flask as fk
+import html
+from flask import render_template
+import logging
 
-app = Flask(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
-@app.route("/")
+
+app = fk.Flask(__name__)
+
+@app.route("/", methods = ["GET", "POST"])
 def index():
-    return("hello world!")
+    if(fk.request.method == "GET"):
+        return(render_template("studentRequest.html")) #defualt to the student sign up page for when a student scans qr code
+    else:
+        return "you did it!" #post return, should eventually lead to suggested tutors page
 
 if __name__ == "__main__":
     app.run(debug=True)
